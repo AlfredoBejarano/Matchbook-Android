560fed95f30fd985dc88cd4af88ede3dffab8f0a
Alfredo Bejarano <alfredocbejarano@gmail.com>
2019-08-22 22:14:51 -0500

    Update ScorecardRowListTypeConverter.kt

--- app/src/main/java/me/alfredobejarano/golfassistant/data/ScorecardRowListTypeConverter.kt
+++ app/src/main/java/me/alfredobejarano/golfassistant/data/ScorecardRowListTypeConverter.kt
@@ -2,17 +2,19 @@ package me.alfredobejarano.golfassistant.data
 
 import androidx.room.TypeConverter
 import com.google.gson.GsonBuilder
+import com.google.gson.reflect.TypeToken
 
 class ScorecardRowListTypeConverter {
     companion object {
+        private val listType = object : TypeToken<List<ScorecardRow>>() {}.type
         private val gson by lazy { GsonBuilder().excludeFieldsWithoutExposeAnnotation().create() }
 
         @JvmStatic
         @TypeConverter
-        fun convertToStringList(rows: List<ScorecardRow>) = gson.run { rows.map(::toJson) }
+        fun convertToStringList(rows: List<ScorecardRow>): String = gson.toJson(rows)
 
         @JvmStatic
         @TypeConverter
-        fun convertToRowList(rows: List<String>): List<ScorecardRow> = gson.run { rows.map(::fromJson) }
+        fun convertToRowList(rows: String): List<ScorecardRow> = gson.fromJson(rows, listType)
     }
 }
