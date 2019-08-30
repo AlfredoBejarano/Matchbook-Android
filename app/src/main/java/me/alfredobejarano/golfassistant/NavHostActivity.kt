31bb70b9ecb0e8b8ff0fba6bea96709b02cdccae
Alfredo Bejarano <alfredocbejarano@gmail.com>
2019-08-30 13:16:34 -0500

    Create NavHostActivity.kt

---
+++ app/src/main/java/me/alfredobejarano/golfassistant/NavHostActivity.kt
@@ -0,0 +1,12 @@
+package me.alfredobejarano.golfassistant
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NavHostActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_nav_host)
+    }
+}
