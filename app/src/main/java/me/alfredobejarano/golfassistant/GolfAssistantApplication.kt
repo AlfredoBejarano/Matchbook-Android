408115379fe65ad3869f99e740dd6fb5fadf72aa
Alfredo Bejarano <alfredocbejarano@gmail.com>
2019-11-05 14:43:07 -0600

    Update project dependencies

--- app/src/main/java/me/alfredobejarano/golfassistant/GolfAssistantApplication.kt
+++ app/src/main/java/me/alfredobejarano/golfassistant/GolfAssistantApplication.kt
@@ -1,18 +1,16 @@
 package me.alfredobejarano.golfassistant
 
 import android.app.Application
-import androidx.fragment.app.Fragment
-import dagger.android.AndroidInjector
 import dagger.android.DispatchingAndroidInjector
-import dagger.android.support.HasSupportFragmentInjector
+import dagger.android.HasAndroidInjector
 import me.alfredobejarano.golfassistant.injection.Injector
 import javax.inject.Inject
 
-class GolfAssistantApplication : Application(), HasSupportFragmentInjector {
+class GolfAssistantApplication : Application(), HasAndroidInjector {
     @Inject
-    lateinit var fragmentInjector: DispatchingAndroidInjector<Fragment>
+    lateinit var fragmentInjector: DispatchingAndroidInjector<Any>
 
-    override fun supportFragmentInjector(): AndroidInjector<Fragment> = fragmentInjector
+    override fun androidInjector() = fragmentInjector
 
     override fun onCreate() {
         Injector.init(this)
